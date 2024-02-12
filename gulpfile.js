186cb97ffd5be67daa8b5ba88c86f3e4e60c047a
urchifox <126313096+urchifox@users.noreply.github.com>
2024-02-12 13:55:28 +0400

    init project

---
+++ gulpfile.js
@@ -0,0 +1,196 @@
+import { readFileSync, rmSync } from 'node:fs';
+
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import { nunjucksCompile } from 'gulp-nunjucks';
+import htmlmin from 'gulp-htmlmin';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import postcss from 'gulp-postcss';
+import postUrl from 'postcss-url';
+import autoprefixer from 'autoprefixer';
+import csso from 'postcss-csso';
+import { createGulpEsbuild } from 'gulp-esbuild';
+import browserslistToEsbuild from 'browserslist-to-esbuild';
+import sharp from 'gulp-sharp-responsive';
+import svgo from 'gulp-svgmin';
+import { stacksvg } from 'gulp-stacksvg';
+import server from 'browser-sync';
+import bemlinter from 'gulp-html-bemlinter';
+
+const { src, dest, watch, series, parallel } = gulp;
+const sass = gulpSass(dartSass);
+const PATH_TO_SOURCE = './source/';
+const PATH_TO_DIST = './build/';
+const PATH_TO_RAW = './raw/';
+const PATHS_TO_STATIC = [
+  `${PATH_TO_SOURCE}fonts/**/*.{woff2,woff}`,
+  `${PATH_TO_SOURCE}*.ico`,
+  `${PATH_TO_SOURCE}*.webmanifest`,
+  `${PATH_TO_SOURCE}favicons/**/*.{png,svg}`,
+  `${PATH_TO_SOURCE}vendor/**/*`,
+  `${PATH_TO_SOURCE}images/**/*`,
+  `!${PATH_TO_SOURCE}images/icons/**/*`,
+  `!${PATH_TO_SOURCE}**/README.md`,
+];
+let isDevelopment = true;
+
+export function processMarkup () {
+  return src(`${PATH_TO_SOURCE}**/*.html`)
+    .pipe(nunjucksCompile())
+    .pipe(htmlmin({ collapseWhitespace: !isDevelopment }))
+    .pipe(dest(PATH_TO_DIST))
+    .pipe(server.stream());
+}
+
+export function lintBem () {
+  return src(`${PATH_TO_SOURCE}*.html`)
+    .pipe(bemlinter());
+}
+
+export function processStyles () {
+  return src(`${PATH_TO_SOURCE}styles/*.scss`, { sourcemaps: isDevelopment })
+    .pipe(plumber())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([
+      postUrl({ assetsPath: '../' }),
+      autoprefixer(),
+      csso()
+    ]))
+    .pipe(dest(`${PATH_TO_DIST}styles`, { sourcemaps: isDevelopment }))
+    .pipe(server.stream());
+}
+
+export function processScripts () {
+  const gulpEsbuild = createGulpEsbuild({ incremental: isDevelopment });
+
+  return src(`${PATH_TO_SOURCE}scripts/*.js`)
+    .pipe(gulpEsbuild({
+      bundle: true,
+      format: 'esm',
+      // splitting: true,
+      platform: 'browser',
+      minify: !isDevelopment,
+      sourcemap: isDevelopment,
+      target: browserslistToEsbuild(),
+    }))
+    .pipe(dest(`${PATH_TO_DIST}scripts`))
+    .pipe(server.stream());
+}
+
+export function optimizeRaster () {
+  const RAW_DENSITY = 2;
+  const TARGET_FORMATS = [undefined, 'webp']; // undefined — initial format: jpg or png
+
+  function createOptionsFormat() {
+    const formats = [];
+
+    for (const format of TARGET_FORMATS) {
+      for (let density = RAW_DENSITY; density > 0; density--) {
+        formats.push(
+          {
+            format,
+            rename: { suffix: `@${density}x` },
+            width: ({ width }) => Math.ceil(width * density / RAW_DENSITY),
+            jpegOptions: { progressive: true },
+          },
+        );
+      }
+    }
+
+    return { formats };
+  }
+
+  return src(`${PATH_TO_RAW}images/**/*.{png,jpg,jpeg}`)
+    .pipe(sharp(createOptionsFormat()))
+    .pipe(dest(`${PATH_TO_SOURCE}images`));
+}
+
+export function optimizeVector () {
+  return src([`${PATH_TO_RAW}**/*.svg`])
+    .pipe(svgo())
+    .pipe(dest(PATH_TO_SOURCE));
+}
+
+export function createStack () {
+  return src(`${PATH_TO_SOURCE}images/icons/**/*.svg`)
+    .pipe(stacksvg())
+    .pipe(dest(`${PATH_TO_DIST}images/icons`));
+}
+
+export function copyStatic () {
+  return src(PATHS_TO_STATIC, { base: PATH_TO_SOURCE })
+    .pipe(dest(PATH_TO_DIST));
+}
+
+export function startServer () {
+  const serveStatic = PATHS_TO_STATIC
+    .filter((path) => path.startsWith('!') === false)
+    .map((path) => {
+      const dir = path.replace(/(\/\*\*\/.*$)|\/$/, '');
+      const route = dir.replace(PATH_TO_SOURCE, '/');
+
+      return { route, dir };
+    });
+
+  server.init({
+    server: {
+      baseDir: PATH_TO_DIST
+    },
+    serveStatic,
+    cors: true,
+    notify: false,
+    ui: false,
+  }, (err, bs) => {
+    bs.addMiddleware('*', (req, res) => {
+      res.write(readFileSync(`${PATH_TO_DIST}404.html`));
+      res.end();
+    });
+  });
+
+  watch(`${PATH_TO_SOURCE}**/*.{html,njk}`, series(processMarkup));
+  watch(`${PATH_TO_SOURCE}styles/**/*.scss`, series(processStyles));
+  watch(`${PATH_TO_SOURCE}scripts/**/*.js`, series(processScripts));
+  watch(`${PATH_TO_SOURCE}images/icons/**/*.svg`, series(createStack, reloadServer));
+  watch(PATHS_TO_STATIC, series(reloadServer));
+}
+
+function reloadServer (done) {
+  server.reload();
+  done();
+}
+
+export function removeBuild (done) {
+  rmSync(PATH_TO_DIST, {
+    force: true,
+    recursive: true,
+  });
+  done();
+}
+
+export function buildProd (done) {
+  isDevelopment = false;
+  series(
+    removeBuild,
+    parallel(
+      processMarkup,
+      processStyles,
+      processScripts,
+      createStack,
+      copyStatic,
+    ),
+  )(done);
+}
+
+export function runDev (done) {
+  series(
+    removeBuild,
+    parallel(
+      processMarkup,
+      processStyles,
+      processScripts,
+      createStack,
+    ),
+    startServer,
+  )(done);
+}
---
+++ source/fonts/README.md
@@ -0,0 +1,21 @@
+# Папка для шрифтов
+
+```shell
+└── source/
+    └── fonts/
+        ├── pt-sans-400.woff2
+        └── pt-sans-700.woff2
+```
+
+Если в проекте используется несколько шрифтов, то для каждого семейства можно использовать отдельную папку:
+
+```shell
+└── source/
+    └── fonts/
+        ├── pt-sans/
+        │   ├── pt-sans-bold.woff2
+        │   └── pt-sans-bold.woff
+        └── tt-norms/
+            ├── tt-norms-regular.woff2
+            └── tt-norms-regular.woff
+```
