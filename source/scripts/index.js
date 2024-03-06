2d56f821331a6b4810a55efb6d364a098d8d8889
urchifox <126313096+urchifox@users.noreply.github.com>
2024-03-06 20:46:17 +0400

    add script for additipns button

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,4 +1,6 @@
 /* в этот файл добавляет скрипты*/
 import {init as initSchemeToggle} from './scheme-toggle.js';
+import {init as initAdditions} from './additions.js';
 
 initSchemeToggle();
+initAdditions();
