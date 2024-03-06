1ddac34edcd4bf0afd85c96b903ca13fdb9e3ae7
urchifox <126313096+urchifox@users.noreply.github.com>
2024-03-06 20:48:29 +0400

    fix script file name

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,6 +1,6 @@
 /* в этот файл добавляет скрипты*/
 import {init as initSchemeToggle} from './scheme-toggle.js';
-import {init as initAdditions} from './additions.js';
+import {init as initAdditions} from './additions-card.js';
 
 initSchemeToggle();
 initAdditions();
