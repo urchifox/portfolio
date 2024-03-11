9087cfe85d8fd2424a2414ce07f4c380e067904a
urchifox <126313096+urchifox@users.noreply.github.com>
2024-03-11 15:07:21 +0400

    add script for closing header

--- source/scripts/header.js
+++ source/scripts/header.js
@@ -1,8 +1,15 @@
 const element = document.querySelector('.header');
 const height = element.clientHeight;
+const button = element.querySelector('.header__menu-toggle');
+const links = element.querySelectorAll('.header__menu-link');
+
+const onLinkClick = () => {
+  button.checked = false;
+};
 
 const init = () => {
   document.documentElement.style.setProperty('--header-height', `${height}px`);
+  links.forEach((link) => link.addEventListener('click', onLinkClick));
 };
 
 export {init};
