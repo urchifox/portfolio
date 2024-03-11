88352b2f52526ce00bca1a76563297dccdd86809
urchifox <126313096+urchifox@users.noreply.github.com>
2024-03-11 14:57:06 +0400

    correct defining headers height

--- source/scripts/header.js
+++ source/scripts/header.js
@@ -2,7 +2,7 @@ const element = document.querySelector('.header');
 const height = element.clientHeight;
 
 const init = () => {
-  document.documentElement.style.setProperty('--header-height', `${height * 1.33}px`);
+  document.documentElement.style.setProperty('--header-height', `${height}px`);
 };
 
 export {init};
