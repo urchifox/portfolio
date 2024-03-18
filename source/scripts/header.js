1e7e0bc6b129e2a643420f638c55cc2e281c9b39
urchifox <126313096+urchifox@users.noreply.github.com>
2024-03-18 15:39:46 +0400

    make menu wrap if overlay was clicked

--- source/scripts/header.js
+++ source/scripts/header.js
@@ -7,9 +7,16 @@ const onLinkClick = () => {
   button.checked = false;
 };
 
+const onOverlayClick = (evt) => {
+  if (evt.offsetY > element.offsetHeight) {
+    button.checked = false;
+  }
+};
+
 const init = () => {
   document.documentElement.style.setProperty('--header-height', `${height}px`);
   links.forEach((link) => link.addEventListener('click', onLinkClick));
+  element.addEventListener('click', onOverlayClick);
 };
 
 export {init};
