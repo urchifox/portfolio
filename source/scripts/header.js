5c30a78244d695e3ce3bd287dc8f7dd8d3d8392d
urchifox <126313096+urchifox@users.noreply.github.com>
2024-04-28 21:28:18 +0400

    add scroll progress bar

--- source/scripts/header.js
+++ source/scripts/header.js
@@ -1,22 +1,107 @@
-const element = document.querySelector('.header');
-const height = element.clientHeight;
-const button = element.querySelector('.header__menu-toggle');
-const links = element.querySelectorAll('.header__menu-link');
+import {isInTheMiddle, isScrolledToTop, isScrolledToBottom} from './utils.js';
 
-const onLinkClick = () => {
-  button.checked = false;
+const root = document.querySelector('.header');
+const button = root.querySelector('.header__menu-toggle');
+const progressBar = root.querySelector('.header__scroll-indicator');
+const links = root.querySelectorAll('.header__menu-link');
+const linksMap = new Map();
+
+links.forEach((link) => {
+  const id = link.href.substring(link.href.indexOf('#'));
+  const target = document.querySelector(id);
+  linksMap.set(target, link);
+});
+
+let currentTarget = linksMap.keys().next().value;
+
+const setHeaderHeight = () => {
+  const height = root.clientHeight;
+  document.documentElement.style.setProperty('--header-height', `${height}px`);
 };
 
-const onOverlayClick = (evt) => {
-  if (evt.offsetY > element.offsetHeight) {
+const onLinkClick = () => {
+  if (window.innerWidth < 760) {
     button.checked = false;
   }
 };
 
+const onOverlayClick = (evt) => {
+  if (evt.offsetY > root.offsetHeight) {
+    button.checked = false;
+  }
+};
+
+const changeCurrentTarget = (newTarget) => {
+  linksMap.get(currentTarget).classList.remove('header__menu-link--current');
+  currentTarget = newTarget;
+  const currentLink = linksMap.get(currentTarget);
+  currentLink.classList.add('header__menu-link--current');
+
+  if (window.innerWidth >= 760) {
+    const currentLinkRect = currentLink.getBoundingClientRect();
+    progressBar.style.left = `${currentLinkRect.x}px`;
+    progressBar.style.width = `${currentLinkRect.width}px`;
+  }
+};
+
+const setProgressBar = () => {
+  const scrollPosition = document.documentElement.scrollTop;
+  const pageHeight = document.body.scrollHeight - window.innerHeight;
+  const progress = (scrollPosition / pageHeight) * 100;
+  progressBar.style.setProperty('--scroll-percent', `${progress}%`);
+};
+
+const onPageScroll = () => {
+  if (window.innerWidth < 760) {
+    setProgressBar();
+  }
+
+  if (isScrolledToTop()) {
+    changeCurrentTarget(linksMap.keys().next().value);
+
+    return;
+  }
+
+  if (isScrolledToBottom()) {
+    changeCurrentTarget([...linksMap.keys()].at(-1));
+
+    return;
+  }
+
+  if (isInTheMiddle(currentTarget)) {
+    return;
+  }
+
+  for (const [target] of linksMap) {
+    if (isInTheMiddle(target)) {
+      changeCurrentTarget(target);
+
+      return;
+    }
+  }
+};
+
+const onPageResize = () => {
+  setHeaderHeight();
+
+  if (window.innerWidth < 760) {
+    progressBar.style = '';
+    setProgressBar();
+
+    return;
+  }
+
+  changeCurrentTarget(currentTarget);
+};
+
 const init = () => {
-  document.documentElement.style.setProperty('--header-height', `${height}px`);
+  setHeaderHeight();
+  changeCurrentTarget(currentTarget);
+
   links.forEach((link) => link.addEventListener('click', onLinkClick));
-  element.addEventListener('click', onOverlayClick);
+  root.addEventListener('click', onOverlayClick);
+  window.addEventListener('scroll', onPageScroll);
+  window.addEventListener('resize', onPageResize);
 };
 
 export {init};
--- source/scripts/utils.js
+++ source/scripts/utils.js
@@ -1,6 +1,22 @@
-export function createElement(template) {
+export const createElement = (template) => {
   const newElement = document.createElement('div');
   newElement.innerHTML = template;
 
   return newElement.firstElementChild;
-}
+};
+
+export const isInTheMiddle = (element) => {
+  const elementRect = element.getBoundingClientRect();
+  const viewportMidpoint = window.innerHeight / 2;
+
+  return elementRect.top <= viewportMidpoint && elementRect.bottom >= viewportMidpoint;
+};
+
+export const isScrolledToTop = () => document.documentElement.scrollTop < 10;
+
+export const isScrolledToBottom = () => {
+  const scrollPosition = document.documentElement.scrollTop;
+  const pageHeight = document.body.scrollHeight - window.innerHeight;
+
+  return pageHeight - scrollPosition < 10;
+};
