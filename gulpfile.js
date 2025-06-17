9afe84ae7ef5bcbcbd640b7b6e9c8df4ea9e2b34
urchifox <126313096+urchifox@users.noreply.github.com>
2025-06-17 22:42:52 +0400

    rename blocks -> sections and val -> section in header.pug

--- gulpfile.js
+++ gulpfile.js
@@ -46,12 +46,12 @@ export function processMarkup () {
 }
 
 export function compilePug() {
-  const blocks = JSON.parse(fs.readFileSync(`${PATH_TO_SOURCE}data/sections.json`));
+  const sections = JSON.parse(fs.readFileSync(`${PATH_TO_SOURCE}data/sections.json`));
   const contacts = JSON.parse(fs.readFileSync(`${PATH_TO_SOURCE}data/contacts.json`));
   return src(`${PATH_TO_SOURCE}pug/*.pug`)
     .pipe(pug({
       pretty: true, // чтобы разметка была не минифицирована
-      locals: { blocks, contacts },
+      locals: { sections, contacts },
     }))
     .pipe(dest(PATH_TO_DIST));
 }
